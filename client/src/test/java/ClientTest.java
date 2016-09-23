2f7661cb28177239e765d7421413b248298a4e0a
Akhil Sharma <coolakhil.123@example.com>
2016-09-23 05:31:57 +0530

    pushed project

---
+++ client/src/test/java/ClientTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by akhil on 23/9/16.
+ */
+public class ClientTest {
+
+    @Test
+    public void testJunit5() throws Exception {
+        String str = "junit5";
+
+        assertEquals("junit5", str);
+
+    }
+}
